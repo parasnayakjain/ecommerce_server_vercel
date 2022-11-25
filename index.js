4483e83f6e6ccdeaf977933dfb23b59f0aa0555f
parasnayakjain <paras.jain.cd.mec19@itbhu.ac.in>
2022-11-25 16:09:53 +0530

    a

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const dotenv = require("dotenv");
 const cloudinary=require("cloudinary");
 dotenv.config({ path: "server/config/config.env" });
 const PORT = process.env.PORT;
+const express = require("express");
+const app2=express();
 
-
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-});
+app2.listen(3000, ()=>{
+    console.log('listening on 3000')
+})
+// app.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`)
+// });
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
